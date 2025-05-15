d8abdec33bd32e54b353859dc2ab0324d7c35bd4
AnaisAtos <100564562+AnaisAtos@users.noreply.github.com>
2025-05-15 18:00:46 +0200

    Update data_tools_test.py
    added self to tests

--- src/baseline_model/test/data_tools_test.py
+++ src/baseline_model/test/data_tools_test.py
@@ -18,38 +18,38 @@ class TestSplitTrainTest:
 
     @it("must take a split proportion 0<=prop<=1 otherwise raise a value error")
     @skip
-    def test_prop_error():
+    def test_prop_error(self):
         pass
 
     @it("must take a valid dataset path otherwise raise a value error")
     @skip
-    def test_dataset_path():
+    def test_dataset_path(self):
         pass
 
     @it(
         "must split the dataset into two complementary train and val sets len(train)+len(val)=len(dataset)"
     )
     @skip
-    def test_len_dataset():
+    def test_len_dataset(self):
         pass
 
     @it(
         "must return two datasets containing only the specified classes in the input parameters"
     )
     @skip
-    def test_len_datasets():
+    def test_len_datasets(self):
         pass
 
     @it(
         "must return a train dataset with num_images_train ~= (1-prop)*total_num_images"
     )
     @skip
-    def test_len_train_dataset():
+    def test_len_train_dataset(self):
         pass
 
     @it("must return a val dataset with num_images_val ~= prop*total_num_images")
     @skip
-    def test_len_val_dataset():
+    def test_len_val_dataset(self):
         pass
 
 
@@ -58,27 +58,27 @@ class TestCustomDataset:
 
     @it("must return an int")
     @skip
-    def test_len_output():
+    def test_len_output(self):
         pass
 
     @it("must load an image in PIL format")
     @skip
-    def test_loaded_image():
+    def test_loaded_image(self):
         pass
 
     @it("must return int label")
     @skip
-    def test_loaded_label():
+    def test_loaded_label(self):
         pass
 
     @it("must return an image transformed to size 224*224")
     @skip
-    def test_image_resize_transform():
+    def test_image_resize_transform(self):
         pass
 
     @it("must return an image as tensor")
     @skip
-    def test_image_tensor_transform():
+    def test_image_tensor_transform(self):
         pass
 
 
@@ -89,12 +89,12 @@ class TestResnet50Preprocessing:
         "must return a dataloader of batches with size (BATCH_SIZE, 3, 224, 224), (BATCH_SIZE,)"
     )
     @skip
-    def test_batch_shape():
+    def test_batch_shape(self):
         pass
 
     @it(
         "must return a dataloader with NUM_BATCHES = NUM_IMAGES//BATCH_SIZE (+1 if not divisible)"
     )
     @skip
-    def test_dataloader_len():
+    def test_dataloader_len(self):
         pass
